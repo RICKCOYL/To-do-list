b53501227866146e36072eb675cf4a08022670b1
RICKCOYL <rickoburu@gmail.com>
2021-02-18 09:19:44 +0300

    create new branch

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import 'bootstrap/js/dist/collapse';
+import './components/style.css';
+import Project from './components/projectconstructor';
+import domModule from './components/dom';
+import { ProjectForm, TaskForm } from './components/logic';
+import { updateLocalStorage } from './components/module';
+
+document.getElementById('submit-task-form-button').addEventListener('click', () => {
+  TaskForm();
+
+  const tasksList = JSON.parse(localStorage.getItem('selected project')).tasks || [];
+  const tasksColumn = document.querySelector('.task-list');
+  domModule.populateList(tasksColumn, tasksList, domModule.displayTask);
+  document.getElementById('display-task-form-btn').click();
+});
+
+document
+  .getElementById('submit-project-form-button')
+  .addEventListener('click', () => {
+    ProjectForm();
+    const projectsList = JSON.parse(localStorage.getItem('projects')) || [];
+    const projectsColumn = document.querySelector('.project-list');
+
+    domModule.populateList(projectsColumn, projectsList, domModule.displayProject);
+    document.getElementById('display-form-btn').click();
+  });
+
+const projects = JSON.parse(localStorage.getItem('projects'));
+const selectedProject = JSON.parse(localStorage.getItem('selected-project'));
+
+if (projects === null) {
+  const seedProject = new Project('Default Project', 'This is a project to get you started. Feel free to change the name, description and date!', '01/01/3000');
+  updateLocalStorage([
+    ['projects', JSON.stringify([seedProject])],
+  ]);
+}
+if ([null, []].includes(selectedProject)) {
+  const firstProject = JSON.parse(localStorage.getItem('projects'))[0];
+  updateLocalStorage([
+    ['selected project', JSON.stringify(firstProject)],
+  ]);
+}
+
+domModule.refreshLists();
